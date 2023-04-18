4bad352bb7201c0c7c56624498cb1a23a9e492e3
Roberto Morais <robmsjr@gmail.com>
2023-04-18 11:28:28 +0200

    POC version

--- main.js
+++ main.js
@@ -1,3 +1,65 @@
+async function submitForm(event) {
+  event.preventDefault();
+
+  const bookType = document.getElementById("book-type").value;
+  const genre = document.getElementById("genre").value;
+  const examples = document.getElementById("examples").value;
+
+  // Disable the submit button and show the loading spinner
+  const submitButton = document.querySelector("button[type='submit']");
+  const loadingSpinner = document.getElementById("loading-spinner");
+  submitButton.disabled = true;
+  loadingSpinner.style.display = "inline-block";
+
+  const recommendations = await getRecommendations(bookType, genre, examples);
+
+  // Enable the submit button and hide the loading spinner
+  submitButton.disabled = false;
+  loadingSpinner.style.display = "none";
+
+  displayRecommendations(recommendations);
+}
+
+
+async function getRecommendations(bookType, genre, examples) {
+  const response = await fetch('/get-recommendations', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          bookType,
+          genre,
+          examples,
+      }),
+  });
+
+  const recommendations = await response.json();
+  return recommendations;
+}
+
+
+function displayRecommendations(recommendations) {
+  const recommendationsDiv = document.getElementById('recommendations');
+  recommendationsDiv.innerHTML = '';
+  
+  // const paragraph = document.createElement('p');
+  // let text = document.createTextNode(recommendations);
+  // paragraph.appendChild(text);
+  // recommendationsDiv.append(paragraph);
+  
+  recommendations.forEach(rec => {
+      const bookLink = document.createElement('a');
+      bookLink.href = `https://www.goodreads.com/book/show/${rec.goodreadsID}`;
+      bookLink.target = '_blank';
+      bookLink.textContent = rec.title;
+      recommendationsDiv.appendChild(bookLink);
+      recommendationsDiv.appendChild(document.createElement('br'));
+  });
+}
+
+
+// old
 async function fetchBooks() {
   const response = await fetch('/books');
   const books = await response.json();
@@ -25,7 +87,7 @@ async function searchBooks() {
   displayBooks(filteredBooks);
 }
 
-window.onload = async function() {
-  const books = await fetchBooks();
-  displayBooks(books);
-};
+// window.onload = async function() {
+//   const books = await fetchBooks();
+//   displayBooks(books);
+// };
--- server.js
+++ server.js
@@ -1,14 +1,90 @@
+require('dotenv').config();
+
 const express = require('express');
-const app = express();
 const db = require('./db');
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
 // app.get('/', (req, res) => {
 //    res.send('Hello, World!');
 // });
+
+app.post('/get-recommendations', async (req, res) => {
+    const { bookType, genre, examples } = req.body;
+
+    try {
+        const prompt = `I am looking for ${bookType} book recommendations in the ${genre} genre. Some examples of books I've enjoyed are: ${examples}. Can you give me 6 books suggestions based on this information?I want you to return with the following pattern:
+        <Num>. <Book Name>|<GoodReads ID>|<Amazon ASIN>\n
+        Example:
+        1.Ender's Game|375802|0812550706\n`;
+        console.log("1");
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: prompt}],
+            });
+        //   console.log(completion.data.choices[0].message);
+          
+        // const response = await openai.createCompletion({
+        //     model: "gpt-3.5-turbo",
+        //     prompt: prompt,
+        //     max_tokens: 150,
+        //     temperature: 0.7,
+        //     // top_p: 1,
+        //     // n: 1,
+        //     // //stream: false,
+        //     //logprobs: null,
+        //     // stop: "\n",
+        //   });
+        console.log("2");
+        
+
+        // console.log("results:");
+        // console.log(response.data);
+        // console.log("--- end of results");
+        console.log(response.data.choices[0].message.content);
+        const recommendations = parseRecommendations(response.data.choices[0].message.content);
+        console.log("4");
+        res.json(recommendations);
+    } catch (err) {
+        console.log("err:");
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+function parseRecommendations(responseText) {
+    // Implement a parsing function based on the expected response format
+    // For example, if you expect the response to be a comma-separated list of book titles, you can do:
+    // const titles = responseText.split(',').map(title => title.trim());
+    // You can then search for Goodreads URLs for each title and return an array of objects containing titles and URLs
+    // return [];
+    // Split the text into an array of lines
+    console.log("3");
+    const lines = responseText.split('\n').filter(line => line.trim() !== '');
+    const books = lines.map(line => {
+        const parts = line.split('|');
+        const title = parts[0].replace(/^\d+\. /, '').trim();
+        const goodreadsID = parts[1].trim();
+        const amazonASIN = parts[2].trim();
+
+        return { title, goodreadsID, amazonASIN };
+    });
+    console.log(books);
+    return books;
+  }
+
 
 // Endpoint to serve the list of books from the database
 app.get('/books', async (req, res) => {
